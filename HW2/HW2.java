b5cc696d2f12dba2c5c4c901178890b2f0e0641e
aidanj123 <38387090+aidanj123@users.noreply.github.com>
2025-02-24 13:23:19 -0500

    paths

---
+++ HW2/HW2.java
@@ -0,0 +1,35 @@
+package HW2;
+import java.util.Scanner;
+
+public class HW2 {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the input size: ");
+        int n = scanner.nextInt();
+        System.out.println("Enter the number of repetitions: ");
+        int r = scanner.nextInt();
+
+        int[] A1 = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int x = (int) ((Math.random() * 999999));
+            int y = (int) ((Math.random() * (2 * r) + 1));
+
+            for (int j = 0; j < y && i < n; j++) {
+                A1[i] = x;
+                if (j < y - 1) {
+                    i++;
+                }
+            }
+            i++;
+        }
+
+        int[] A2 = A1;
+
+        System.out.println(A1);
+        System.out.println(A2);
+
+        scanner.close();
+    }
+}
