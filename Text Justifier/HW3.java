8f63f0ca7bce5a593b43c5c9fd744a39c51de60b
aidanj123 <38387090+aidanj123@users.noreply.github.com>
2025-04-07 14:28:20 -0400

    Steps 1-2

--- Text Justifier/HW3.java
+++ Text Justifier/HW3.java
@@ -6,7 +6,7 @@ public class HW3 {
                     "lives" , "are" , "beginning" , "to" , "erode" , "and" , "awkward" , "situations" , "have" , "ensued." }; 
     static int n = W.length;
     static int i = 0;
-    //W.length = 44 
+    //W.length = 44
     static int j = 5;
 
 
@@ -25,10 +25,7 @@ public class HW3 {
         }
         return l - 1;
     }
-    // public static int[] split(int w, String[] W){
-    //     int[] split = new int[W.length];
 
-    // }
     public static int[] minBadness(String[] W, int w){
         int[] memo = new int[W.length];
         int[] linebreaksMemo = new int[W.length];
@@ -47,10 +44,10 @@ public class HW3 {
             linebreaksMemo[i] = n;
         }
         else {
-            int min = Integer.MAX_VALUE;
-            int indexMin = 0;
+            double min = Integer.MAX_VALUE;
+            double indexMin = 0;
             for (int j = i+1; j < n; j++){
-                int temp = badness(W, i, j, w);
+                double temp = badness(W, i, j, w);
                 temp ++;
                 memorizedMinimumBadness(W, j, memo, linebreaksMemo, w);
                 if (temp < min){
@@ -58,13 +55,38 @@ public class HW3 {
                     indexMin = j;
                 }
             }
-            memo[i] = min;
-            linebreaksMemo[i] = indexMin;
+            memo[i] = (int) min;
+            linebreaksMemo[i] = (int) indexMin;
         }
         return memo[i];
     }
 
+    //  This method returns an array of integers where each element represents the index of the next line break.
+    //  The first element is how man words are in the first line, and the second element is how many words are in the second line, and so on.
+    //  Once all words are in the array, the array will be filled with -1 which signal to further methods that all words are accounted for.
+    public static int[] split(int w, String[] W){
+        int[] L = new int[W.length];
+        for (int i = 0; i < W.length; i++){
+            L[i] = -1;
+        }
+        int[] minBadness = minBadness(W, w);
+        int next = minBadness[0];
+        int total = 0;
+        for (int i = 0; i < W.length; i++){
+            if (total > W.length){
+                break;
+            }
+            total += next;
+            L[i] = next;
+            next = minBadness[next];
+        }    
+        return L;
+    }
+
     public static void main(String[] args) {
-        System.out.println(minBadness(W, w));
+        int[] split = split(w, W);
+        for (int i : split) {
+            System.out.println(i);
+        } 
     }
 }
